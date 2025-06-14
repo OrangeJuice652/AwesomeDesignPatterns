835c0acb7c8db971bc51f6ae4669b48c3cfe5b18
suetake_takuro <TakuroSuetake>
2025-06-14 14:24:42 +0900

    history.py

---
+++ command/undo_redo/history.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class History:
+    def __init__(self):
+        self.current = 0
+        self._command_history: List[ICommand]
+
+    def pop_last_command(self):
+        return self._command_history.pop(-1)
+
+    def last_command(self):
+        return self._command_history[-1]
+
+    def append_history(self, command: ICommand):
+        self._command_history.append(command)
+
